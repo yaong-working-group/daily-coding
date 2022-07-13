a1dec5646e1acba7b5c4fe54a285195f28c2c358
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-13 10:09:39 +0900

    Feat : "Daily15.js"

--- JEONGHWANMIN/daily15.js
+++ JEONGHWANMIN/daily15.js
@@ -18,6 +18,8 @@ const powerSet = function (str) {
 
   let result = [];
 
+  // |순서|
+  // [] [c] [b] [b,c] [a] [a,c] [a,b] [a,b,c]
   function dfs(i, arr) {
     // 탈출 조건
     // 만약 문자열 길이랑 i 값이 같으면 배열에 문자를 집어넣고 탈출한다.
@@ -35,6 +37,7 @@ const powerSet = function (str) {
   // 0 부터 시작해서 시작해서 탐색을 시작한다.
   dfs(0, []);
 
+  console.log(result);
   // 탐색이 끝난 배열을 정답 형식에 맞게 전처리 한다.
   let answer = [];
   for (let ar of result) {
@@ -44,3 +47,7 @@ const powerSet = function (str) {
   // 전처리한 배열을 정렬해서 리턴한다.
   return answer.sort();
 };
+
+//
+let output1 = powerSet('abc');
+console.log(output1); // ['', 'a', 'ab', 'abc', 'ac', 'b', 'bc', 'c']
