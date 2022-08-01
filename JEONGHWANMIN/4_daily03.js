5aa362f70d046a300c324f6e00c700da581b3343
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-01 14:50:45 +0900

    Feat : "추가 DP로직 추가"

--- JEONGHWANMIN/4_daily03.js
+++ JEONGHWANMIN/4_daily03.js
@@ -1,15 +1,25 @@
-const LSCS2 = function (arr) {
-  let subArrSum = arr[0];
-  let max = arr[0]; // 정답의 후보를 저장
-  for (let i = 1; i < arr.length; i++) {
-    subArrSum = Math.max(subArrSum + arr[i], arr[i]);
-    max = Math.max(max, subArrSum);
-  }
+/* 
+========== LSCS =============
 
-  return max;
+인터넷 접근 방법 참고...
+
+====================================
+*/
+const LSCS = function (arr) {
+  // 각각 현재 부분 수열의 부분합과 최댓값을 담아줄 변수 선언
+  // Dp에 배열의 첫번 째 값 할당
+  let dp = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    // 만약 이전 배열의 합이 현재 값보다 크다면 더했을 때 더 큰 값이 되기 때문에 dp[i] 값에 (그전 배열 연속된 값) + (현재 배열 값) 을 할당 해준다.
+    if (dp[i - 1] + arr[i] > arr[i]) dp[i] = dp[i - 1] + arr[i];
+    // 이전 배열의 합보다 현재 값이 더 크다면 이전 배열 값의 합이 음수일 것이기 때문에 그전 값은 버리고 현재 값으로 초기화 시켜준다.
+    else dp[i] = arr[i];
+  }
+  // 각각 자리마다의 부분 배열의 합이 담겨 있을 거기 때문에 그중에서 제일 큰 값을 리턴해 주면 된다.
+  return Math.max(...dp);
 };
 
-let output = LSCS2([-1, 2, 3, 5, -6, 5]);
+let output = LSCS2([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
 console.log(output); // --> 6
 
 /* 
@@ -27,21 +37,21 @@ npm ERR! Test failed.  See above for more details.
 ====================================
 */
 
-// const LSCS = function (arr) {
-//   const result = [];
-//   // 모든 부분 집합 배열을 구한다.
-//   for (let i = 0; i < arr.length; i++) {
-//     let temp = [];
-//     for (let j = i; j < arr.length; j++) {
-//       temp.push(arr[j]);
-//       result.push([...temp]);
-//     }
-//   }
-//   let answer = [];
-//   // 구했던 모든 부분 집합 배열 합을 구해서 answer라는 배열에 넣어준다.
-//   for (let el of result) {
-//     answer.push(el.reduce((acc, cur) => acc + cur));
-//   }
-//   // 제일 큰 값을 리턴한다.
-//   return Math.max(...answer);
-// };
+const LSCS2 = function (arr) {
+  const result = [];
+  // 모든 부분 집합 배열을 구한다.
+  for (let i = 0; i < arr.length; i++) {
+    let temp = [];
+    for (let j = i; j < arr.length; j++) {
+      temp.push(arr[j]);
+      result.push([...temp]);
+    }
+  }
+  let answer = [];
+  // 구했던 모든 부분 집합 배열 합을 구해서 answer라는 배열에 넣어준다.
+  for (let el of result) {
+    answer.push(el.reduce((acc, cur) => acc + cur));
+  }
+  // 제일 큰 값을 리턴한다.
+  return Math.max(...answer);
+};
